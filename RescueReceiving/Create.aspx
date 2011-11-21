15ff74a23fe0538ba1776edea997759b2c6646ce
unknown <lopezjo@MACBOOKPRO-WIN.(none)>
2011-11-21 11:27:13 -0500

    Committer: unknown <lopezjo@MACBOOKPRO-WIN.(none)>
     On branch master
     Your branch is ahead of 'origin/master' by 7 commits.
    
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   RRDataLayer/RREmergencyCall.cs
    	modified:   RescueReceiving/App_Data/emergency_call_database.mdf
    	modified:   RescueReceiving/App_Data/emergency_call_database_log.LDF
    	modified:   RescueReceiving/Create.aspx
    	modified:   RescueReceiving/Create.aspx.cs
    	modified:   RescueReceiving/Create.aspx.designer.cs
    	modified:   RescueReceiving/Global.asax.cs

--- RescueReceiving/Create.aspx
+++ RescueReceiving/Create.aspx
@@ -198,40 +198,40 @@
                 <td>
                     <asp:Label ID="Label21" runat="server" Text="Driver:"></asp:Label>
                     <asp:DropDownList ID="ddlDriver" runat="server">
-                        <asp:ListItem>Unrestrained</asp:ListItem>
-                        <asp:ListItem>Restrained</asp:ListItem>
+                        <asp:ListItem Value="0">Unrestrained</asp:ListItem>
+                        <asp:ListItem Value="1">Restrained</asp:ListItem>
                     </asp:DropDownList>
                 </td>
                 <td>
                     <asp:Label ID="Label22" runat="server" Text="Passenger:"></asp:Label>
                     <asp:DropDownList ID="ddlPassenger" runat="server">
-                        <asp:ListItem>Front Unrestrained</asp:ListItem>
-                        <asp:ListItem>Front Restrained</asp:ListItem>
-                        <asp:ListItem>Rear Unrestrained</asp:ListItem>
-                        <asp:ListItem>Rear Restrained</asp:ListItem>
+                        <asp:ListItem Value="0">Front Unrestrained</asp:ListItem>
+                        <asp:ListItem Value="1">Front Restrained</asp:ListItem>
+                        <asp:ListItem Value="2">Rear Unrestrained</asp:ListItem>
+                        <asp:ListItem Value="3">Rear Restrained</asp:ListItem>
                     </asp:DropDownList>
                 </td>
             </tr>
             <tr valign="top">
                 <td>
-                    <asp:CheckBox ID="rbEjected" runat="server" Text="Ejected" />
+                    <asp:CheckBox ID="cbEjected" runat="server" Text="Ejected" />
                 </td>
                 <td>
-                    <asp:CheckBox ID="rbEntrapped" runat="server" Text="Entrapped" />
+                    <asp:CheckBox ID="cbEntrapped" runat="server" Text="Entrapped" />
                 </td>
                 <td>
-                    <asp:CheckBox ID="rbRollover" runat="server" Text="Rollover" />
+                    <asp:CheckBox ID="cbRollover" runat="server" Text="Rollover" />
                 </td>
             </tr>
             <tr valign="top">
                 <td>
-                    <asp:CheckBox ID="rbAirbag" runat="server" Text="Airbag" />
+                    <asp:CheckBox ID="cbAirbag" runat="server" Text="Airbag" />
                 </td>
                 <td>
-                    <asp:CheckBox ID="rbPackaged" runat="server" Text="Packaged" />
+                    <asp:CheckBox ID="cbPackaged" runat="server" Text="Packaged" />
                 </td>
                 <td>
-                    <asp:CheckBox ID="rbHelmet" runat="server" Text="Helmet" />
+                    <asp:CheckBox ID="cbHelmet" runat="server" Text="Helmet" />
                 </td>
             </tr>
         </table>
@@ -280,13 +280,13 @@
             </tr>
             <tr valign="top">
                 <td>
-                    <asp:CheckBox ID="rbStrokeAlert" runat="server" Text="Stroke Alert" />
+                    <asp:CheckBox ID="cbStrokeAlert" runat="server" Text="Stroke Alert" />
                 </td>
                 <td>
-                    <asp:CheckBox ID="rbStemi" runat="server" Text="STEMI" />
+                    <asp:CheckBox ID="cbStemi" runat="server" Text="STEMI" />
                 </td>
                 <td>
-                    <asp:CheckBox ID="rbTraumaAlert" runat="server" Text="Trauma Alert" />
+                    <asp:CheckBox ID="cbTraumaAlert" runat="server" Text="Trauma Alert" />
                 </td>
             </tr>
             <tr valign="top">
@@ -331,7 +331,8 @@
                     <br />
                     <br />
                     <asp:Label ID="Label31" runat="server" Text="ETA:"></asp:Label>
-                    <asp:TextBox ID="tbETA" runat="server"></asp:TextBox>
+                    <asp:DropDownList ID="ddlETA" runat="server">
+                    </asp:DropDownList>
                 </td>
             </tr>
         </table>
@@ -361,10 +362,10 @@
                 </td>
                 <td>
                     <asp:Label ID="Label38" runat="server" Text="DEA#:"></asp:Label>
-                    <asp:TextBox ID="TextBox25" runat="server"></asp:TextBox>
+                    <asp:TextBox ID="tbDEA" runat="server"></asp:TextBox>
                 </td>
                 <td>
-                    <asp:CheckBox ID="rbNarc" runat="server" Text="NARC" />
+                    <asp:CheckBox ID="cbNarc" runat="server" Text="NARC" />
                 </td>
             </tr>
         </table>
--- RescueReceiving/Create.aspx.designer.cs
+++ RescueReceiving/Create.aspx.designer.cs
@@ -553,58 +553,58 @@ namespace RescueReceiving {
         protected global::System.Web.UI.WebControls.DropDownList ddlPassenger;
         
         /// <summary>
-        /// rbEjected control.
+        /// cbEjected control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbEjected;
+        protected global::System.Web.UI.WebControls.CheckBox cbEjected;
         
         /// <summary>
-        /// rbEntrapped control.
+        /// cbEntrapped control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbEntrapped;
+        protected global::System.Web.UI.WebControls.CheckBox cbEntrapped;
         
         /// <summary>
-        /// rbRollover control.
+        /// cbRollover control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbRollover;
+        protected global::System.Web.UI.WebControls.CheckBox cbRollover;
         
         /// <summary>
-        /// rbAirbag control.
+        /// cbAirbag control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbAirbag;
+        protected global::System.Web.UI.WebControls.CheckBox cbAirbag;
         
         /// <summary>
-        /// rbPackaged control.
+        /// cbPackaged control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbPackaged;
+        protected global::System.Web.UI.WebControls.CheckBox cbPackaged;
         
         /// <summary>
-        /// rbHelmet control.
+        /// cbHelmet control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbHelmet;
+        protected global::System.Web.UI.WebControls.CheckBox cbHelmet;
         
         /// <summary>
         /// Label32 control.
@@ -679,31 +679,31 @@ namespace RescueReceiving {
         protected global::System.Web.UI.WebControls.Label Label4;
         
         /// <summary>
-        /// rbStrokeAlert control.
+        /// cbStrokeAlert control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbStrokeAlert;
+        protected global::System.Web.UI.WebControls.CheckBox cbStrokeAlert;
         
         /// <summary>
-        /// rbStemi control.
+        /// cbStemi control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbStemi;
+        protected global::System.Web.UI.WebControls.CheckBox cbStemi;
         
         /// <summary>
-        /// rbTraumaAlert control.
+        /// cbTraumaAlert control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbTraumaAlert;
+        protected global::System.Web.UI.WebControls.CheckBox cbTraumaAlert;
         
         /// <summary>
         /// cbResusitation control.
@@ -823,13 +823,13 @@ namespace RescueReceiving {
         protected global::System.Web.UI.WebControls.Label Label31;
         
         /// <summary>
-        /// tbETA control.
+        /// ddlETA control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox tbETA;
+        protected global::System.Web.UI.WebControls.DropDownList ddlETA;
         
         /// <summary>
         /// Label5 control.
@@ -904,22 +904,22 @@ namespace RescueReceiving {
         protected global::System.Web.UI.WebControls.Label Label38;
         
         /// <summary>
-        /// TextBox25 control.
+        /// tbDEA control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox25;
+        protected global::System.Web.UI.WebControls.TextBox tbDEA;
         
         /// <summary>
-        /// rbNarc control.
+        /// cbNarc control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox rbNarc;
+        protected global::System.Web.UI.WebControls.CheckBox cbNarc;
         
         /// <summary>
         /// btnSubmit control.
