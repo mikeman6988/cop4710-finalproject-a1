57f5495d03f66eb9bab1fcf82dab44590d3b7b6b
unknown <lopezjo@MACBOOKPRO-WIN.(none)>
2011-11-23 15:59:51 -0500

    Committer: unknown <lopezjo@MACBOOKPRO-WIN.(none)>
     On branch master
     Your branch is ahead of 'origin/master' by 8 commits.
    
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   RescueReceiving/App_Data/emergency_call_database.mdf
    	modified:   RescueReceiving/App_Data/emergency_call_database_log.LDF
    	modified:   RescueReceiving/RescueReceiving.csproj
    	new file:   RescueReceiving/Roles/ManageUsers.aspx
    	new file:   RescueReceiving/Roles/ManageUsers.aspx.cs
    	new file:   RescueReceiving/Roles/ManageUsers.aspx.designer.cs
    	modified:   RescueReceiving/Site.Master
    	modified:   RescueReceiving/Site.Master.cs
    	modified:   RescueReceiving/Web.config

---
+++ RescueReceiving/Roles/ManageUsers.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" 
+    CodeBehind="ManageUsers.aspx.cs" Inherits="RescueReceiving.Administration.ManageUsers" %>
+
+<asp:Content ID="HeaderContent" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Table ID="tblUsers" runat="server" GridLines="Vertical" 
+        HorizontalAlign="Center" Width="100%">
+        <asp:TableRow runat="server" BackColor="Black" ForeColor="White" 
+            HorizontalAlign="Center">
+            <asp:TableCell runat="server">Name</asp:TableCell>
+            <asp:TableCell runat="server">Created</asp:TableCell>
+            <asp:TableCell runat="server">Last On</asp:TableCell>
+            <asp:TableCell runat="server">Administrator</asp:TableCell>
+            <asp:TableCell runat="server">Commands</asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="User Name:"></asp:Label>
+    <br />
+    <asp:TextBox ID="tbUserName" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="E-mail:"></asp:Label>
+    <br />
+    <asp:TextBox ID="tbEmail" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label3" runat="server" Text="Password:"></asp:Label>
+    <br />
+    <asp:TextBox ID="tbPassword" runat="server"></asp:TextBox>
+    <br />
+    <asp:CheckBox ID="cbIsAdmin" runat="server" />
+    <br />
+    <br />
+    <asp:Button ID="btnCreateNewUser" runat="server" Text="Create New User" />
+</asp:Content>
---
+++ RescueReceiving/Roles/ManageUsers.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace RescueReceiving.Administration
+{
+    public partial class ManageUsers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Roles, AddUserToRule, RemoveUserFromRole, IsUserInRole
+            // GetRolesForUser
+            MembershipUserCollection members = Membership.GetAllUsers();
+            foreach (MembershipUser member in members)
+            {
+                // New row for table
+                //
+                var row = new TableRow();
+                
+                // User name
+                //
+                var cell = new TableCell();
+                cell.Text = member.UserName;
+                row.Cells.Add(cell);
+
+                // Creation date
+                //
+                cell = new TableCell();
+                cell.Text = member.CreationDate.ToShortDateString();
+                cell.HorizontalAlign = HorizontalAlign.Center;
+                row.Cells.Add(cell);
+
+                // Last online date
+                //
+                cell = new TableCell();
+                cell.Text = member.LastLoginDate.ToShortDateString();
+                cell.HorizontalAlign = HorizontalAlign.Center;
+                row.Cells.Add(cell);
+
+                // Is admin?
+                //
+                cell = new TableCell();
+                cell.HorizontalAlign = HorizontalAlign.Center;
+
+                var checkbox = new CheckBox();
+                checkbox.Text = "Admin";
+                checkbox.Checked = Roles.IsUserInRole(member.UserName, "Admin");
+                
+                cell.Controls.Add(checkbox);
+                row.Cells.Add(cell);
+
+                // Commands
+                //
+                cell = new TableCell();
+                cell.HorizontalAlign = HorizontalAlign.Center;
+                
+                var button = new Button();
+                button.Text = "Update";
+
+                cell.Controls.Add(button);
+                
+                button = new Button();
+                button.Text = "Delete";
+                
+                cell.Controls.Add(button);
+
+                row.Cells.Add(cell);
+
+                tblUsers.Rows.Add(row);
+            }
+        }
+    }
+}
